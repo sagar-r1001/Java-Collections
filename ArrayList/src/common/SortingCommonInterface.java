02c56df2853546cb1125677b773ed010af3ce7e9
Sagar Rathod <sagarrathod6854@gmail.com>
2022-08-02 16:36:53 +0530

    Added selection sort for ArrayList

---
+++ ArrayList/src/common/SortingCommonInterface.java
@@ -0,0 +1,10 @@
+package common;
+
+import java.util.ArrayList;
+
+public interface SortingCommonInterface {
+    /**
+     * @param list It'll sort the input arrayList.
+     */
+    void sort (ArrayList<Integer> list);
+}
---
+++ ArrayList/src/sorting/SelectionSort.java
@@ -0,0 +1,51 @@
+package sorting;
+
+import common.SortingCommonInterface;
+
+import java.util.ArrayList;
+
+/**
+ * This algorithm sorts an array by repeatedly searching for the minimum element in the unsorted sub-array and adds it to the sorted sub-array.
+ */
+public class SelectionSort implements SortingCommonInterface {
+
+    /**
+     * @param list
+     * @inheritDoc
+     */
+    @Override
+    public void sort (ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = minIndex + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            int temporary = list.get(minIndex);
+            list.set(minIndex, list.get(i));
+            list.set(i, temporary);
+        }
+    }
+
+    /**
+     * @param list In sorted array, the objects with equal values will appear in the same order as it was in the unsorted one.
+     */
+    public void stableSort (ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = minIndex + 1; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            int temporary = list.get(minIndex);
+            while (minIndex > i) {
+                list.set(minIndex, list.get(minIndex - 1));
+                minIndex--;
+            }
+
+            list.set(i, temporary);
+        }
+    }
+}
